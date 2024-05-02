01fe66c829a57d911ccc0ebffdfb5fc541cf1b03
illiated <illiated@gmail.com>
2024-05-02 16:10:47 +0100

    Add project files.

---
+++ SwordDamageFormulaGUI/SwordDamage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamageFormulaGUI
+
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage() 
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)   
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage(); // This method undoes flame damage points added if its used after SetFlaming method
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
+        }
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+
+
+    }
+}
